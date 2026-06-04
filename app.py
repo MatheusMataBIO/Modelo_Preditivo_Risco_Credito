a0df0b9418916c2641199174efc292750aa2274a
Matheus Mata <129109445+MatheusMataBIO@users.noreply.github.com>
2026-06-04 02:18:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
+from sklearn.externals import joblib
 import shap
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
